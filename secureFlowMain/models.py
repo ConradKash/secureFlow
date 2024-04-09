18395bee99806b9c8fe0e7b9ccc65fd837ee99f9
ConradKash <kakurucon1234@gmail.com>
2024-04-09 04:07:43 +0300

    Add custom primary key fields and new models

--- secureFlowMain/models.py
+++ secureFlowMain/models.py
@@ -21,12 +21,119 @@ GENDER_CHOICE = (
     (2,_("Female")),
 )
 
+class CustomPrimaryKeyField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('primary_key', True)
+        kwargs.setdefault('max_length', 10)  # Adjust the max length as per your requirement
+        kwargs.setdefault('editable', False)
+        super().__init__(*args, **kwargs)
 
+    def pre_save(self, model_instance, add):
+        if add and not getattr(model_instance, self.attname):
+            # Generate the custom primary key
+            last_object = model_instance.__class__.objects.order_by('-id').first()
+            if last_object and last_object.id:
+                last_id = last_object.id
+                prefix = last_id[:2]
+                number = int(last_id[2:]) + 1
+            else:  # Default prefix
+                number = 1
+            setattr(model_instance, self.attname, f'{prefix}{number:04d}')  # Assuming the number is 4 digits
+
+            return getattr(model_instance, self.attname)
+        else:
+            return super().pre_save(model_instance, add)
+
+
+class Hospital(models.Model):
+    hospital_id = CustomPrimaryKeyField(prefix='hospital_')
+    name = models.CharField(max_length=15, null=True,blank=False)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    license_number = models.CharField(max_length=50)
+    contact_number = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
+    employee_count = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+    established_date = models.DateField()
+    certification_expiry_date = models.DateField()
+    is_certified = models.BooleanField(default=False)
+
+class Pharmacy(models.Model):
+    pharmacy_id = CustomPrimaryKeyField(prefix='pharmacy_')
+    name = models.CharField(max_length=15, null=True,blank=False)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    license_number = models.CharField(max_length=50)
+    contact_number = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
+    employee_count = models.PositiveIntegerField()
+    location = models.CharField(max_length=100)
+    established_date = models.DateField()
+    certification_expiry_date = models.DateField()
+    is_certified = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     user_type = models.CharField(max_length=15, null=True,blank=False)
     address = models.CharField(max_length=256, null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
+    mobile = models.CharField(validators=[phone_regex], max_length=10, blank=True)
+    blood_group = models.IntegerField(choices=BLOOD_GROUP_CHOICE, default=1)
+    gender = models.IntegerField(choices=GENDER_CHOICE, default=1)
+    age = models.PositiveIntegerField(default=0)
+    department = models.CharField(max_length=50, null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='profiles', blank=True)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
+class ProfilePatient(models.Model):
+    patient_id = CustomPrimaryKeyField(prefix='p_')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    email_address = models.CharField(max_length=256, null=True, blank=True)
+    phone_regex = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
+    mobile = models.CharField(validators=[phone_regex], max_length=10, blank=True)
+    blood_group = models.IntegerField(choices=BLOOD_GROUP_CHOICE, default=1)
+    gender = models.IntegerField(choices=GENDER_CHOICE, default=1)
+    age = models.PositiveIntegerField(default=0)
+    profile_pic = models.ImageField(upload_to='profiles', blank=True)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
+class ProfileDoctor(models.Model):
+    doctor_id = CustomPrimaryKeyField(prefix='d_')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hospotal_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    user_type = models.CharField(max_length=15, null=True,blank=False)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    email_address = models.CharField(max_length=256, null=True, blank=True)
+    specialization = models.CharField(max_length=100, null=True, blank=True)
+    experience = models.PositiveIntegerField(default=0)
+    phone_regex = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
+    mobile = models.CharField(validators=[phone_regex], max_length=10, blank=True)
+    blood_group = models.IntegerField(choices=BLOOD_GROUP_CHOICE, default=1)
+    gender = models.IntegerField(choices=GENDER_CHOICE, default=1)
+    age = models.PositiveIntegerField(default=0)
+    department = models.CharField(max_length=50, null=True, blank=True)
+    profile_pic = models.ImageField(upload_to='profiles', blank=True)
+    certification_expiry_date = models.DateField()
+    is_certified = models.BooleanField(default=False)    
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+    
+class ProfileReception(models.Model):
+    doctor_id = CustomPrimaryKeyField(prefix='d_')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    hospotal_id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    user_type = models.CharField(max_length=15, null=True,blank=False)
+    address = models.CharField(max_length=256, null=True, blank=True)
+    email_address = models.CharField(max_length=256, null=True, blank=True)
     phone_regex = RegexValidator(regex=r'^[0-9]\d{9}$', message="Please enter valid mobile number.")
     mobile = models.CharField(validators=[phone_regex], max_length=10, blank=True)
     blood_group = models.IntegerField(choices=BLOOD_GROUP_CHOICE, default=1)
