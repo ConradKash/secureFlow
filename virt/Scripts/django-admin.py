9c83f7d5d044c30ebbf8857ebff81a753999c9f8
john_stuart <lubwamajohnstuart201@gmail.com>
2024-06-17 19:24:18 +0300

    feat:Added blockchain and fixed some ui

---
+++ virt/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\LUBWAMA JOHN STUART\Documents\kakuru\secureFlow\virt\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
